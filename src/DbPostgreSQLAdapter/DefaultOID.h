275cda4bf58e70a4a274a1e2b3da5227a695bf79
jlpeiro <jpeiro@werfen.com>
2024-02-14 15:43:07 +0100

    Added default identifiers for Postgres types

---
+++ src/DbPostgreSQLAdapter/DefaultOID.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace systelab::db::postgresql {
+    enum PostgresqlOID {
+        boolOID = 16,
+        bytearrayOID = 17,
+        charOID = 18,
+        nameOID = 19,
+        smallIntIOD = 21,
+        intOID = 23,
+        textOID = 25,
+        floatOID = 700,
+        doubleOID = 701,
+        varcharOID = 1043,
+        datetimeOID = 1184,
+        pidOID=2206
+    };
+}
