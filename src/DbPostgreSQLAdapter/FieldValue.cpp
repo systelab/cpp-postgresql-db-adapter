04e65a75ba6e4c4f7a03c9ec02fcb671a21b4160
jlpeiro <jpeiro@werfen.com>
2024-02-14 15:40:43 +0100

    Created implementation of fieldValue

---
+++ src/DbPostgreSQLAdapter/FieldValue.cpp
@@ -0,0 +1,495 @@
+#include "stdafx.h"
+#include "FieldValue.h"
+
+namespace{
+	bool isDateTimeNull(const std::chrono::system_clock::time_point& dateTime)
+	{
+		return dateTime == std::chrono::system_clock::time_point();
+	}
+
+	std::chrono::system_clock::time_point stringISOToDateTime(const std::string& dateTime)
+	{
+		std::chrono::system_clock::time_point timePointDateTime;
+		std::istringstream{ dateTime } >> std::chrono::parse("%F %T%z", timePointDateTime);
+		return timePointDateTime;
+	}
+}
+
+namespace systelab::db::postgresql {
+
+	FieldValue::FieldValue(const IField& field)
+		: m_field(field)
+		, m_nullValue(true)
+		, m_default(false)
+		, m_boolValue(false)
+		, m_intValue(0)
+		, m_doubleValue(0.)
+		, m_stringValue("")
+		, m_dateTimeValue()
+		, m_binaryValue()
+	{
+	}
+
+	FieldValue::FieldValue(const IField& field, bool value)
+		: m_field(field)
+		, m_nullValue(false)
+		, m_default(false)
+		, m_boolValue(value)
+		, m_intValue(0)
+		, m_doubleValue(0.)
+		, m_stringValue("")
+		, m_dateTimeValue()
+		, m_binaryValue()
+	{
+		if (m_field.getType() != BOOLEAN)
+		{
+			throw std::runtime_error("Field doesn't accept a boolean value");
+		}
+	}
+
+	FieldValue::FieldValue(const IField& field, int value)
+		: m_field(field)
+		, m_nullValue(false)
+		, m_default(false)
+		, m_boolValue(false)
+		, m_intValue(value)
+		, m_doubleValue(0.)
+		, m_stringValue("")
+		, m_dateTimeValue()
+		, m_binaryValue()
+	{
+		if (m_field.getType() != INT)
+		{
+			throw std::runtime_error("Field doesn't accept an integer value");
+		}
+	}
+
+	FieldValue::FieldValue(const IField& field, double value)
+		: m_field(field)
+		, m_nullValue(false)
+		, m_default(false)
+		, m_boolValue(false)
+		, m_intValue(0)
+		, m_doubleValue(value)
+		, m_stringValue("")
+		, m_dateTimeValue()
+		, m_binaryValue()
+	{
+		if (m_field.getType() != DOUBLE)
+		{
+			throw std::runtime_error("Field doesn't accept a double value");
+		}
+	}
+
+	FieldValue::FieldValue(const IField& field, const std::string& value)
+		: m_field(field)
+		, m_nullValue(false)
+		, m_default(false)
+		, m_boolValue(false)
+		, m_intValue(0)
+		, m_doubleValue(0.)
+		, m_stringValue(value)
+		, m_dateTimeValue()
+		, m_binaryValue()
+	{
+		if (m_field.getType() != STRING)
+		{
+			throw std::runtime_error("Field doesn't accept a string value");
+		}
+	}
+
+	FieldValue::FieldValue(const IField& field, const std::chrono::system_clock::time_point& value)
+		: m_field(field)
+		, m_nullValue(true)
+		, m_default(false)
+		, m_boolValue(false)
+		, m_intValue(0)
+		, m_doubleValue(0.)
+		, m_stringValue("")
+		, m_dateTimeValue()
+		, m_binaryValue()
+	{
+		if (m_field.getType() != DATETIME)
+		{
+			throw std::runtime_error("Field doesn't accept a datetime value");
+		}
+
+		if (!isDateTimeNull(value))
+		{
+			m_dateTimeValue = value;
+			m_nullValue = false;
+		}
+	}
+
+	FieldValue::FieldValue(const IField& field, std::unique_ptr<IBinaryValue> value)
+		: m_field(field)
+		, m_nullValue(false)
+		, m_default(false)
+		, m_boolValue(false)
+		, m_intValue(0)
+		, m_doubleValue(0.)
+		, m_stringValue("")
+		, m_dateTimeValue()
+		, m_binaryValue(std::move(value))
+	{
+		if (m_field.getType() != BINARY)
+		{
+			throw std::runtime_error("Field doesn't accept a binary value");
+		}
+	}
+
+	FieldValue::~FieldValue() = default;
+
+	const IField& FieldValue::getField() const
+	{
+		return m_field;
+	}
+
+	bool FieldValue::isNull() const
+	{
+		return m_nullValue;
+	}
+
+	bool FieldValue::isDefault() const
+	{
+		return m_default;
+	}
+
+	bool FieldValue::getBooleanValue() const
+	{
+		if (isNull())
+		{
+			throw std::runtime_error("Field value is null");
+		}
+
+		if (isDefault())
+		{
+			throw std::runtime_error("Field value is default");
+		}
+
+		if (m_field.getType() != BOOLEAN)
+		{
+			throw std::runtime_error("Field type isn't boolean");
+		}
+
+		return m_boolValue;
+	}
+
+	int FieldValue::getIntValue() const
+	{
+		if (isNull())
+		{
+			throw std::runtime_error("Field value is null");
+		}
+
+		if (isDefault())
+		{
+			throw std::runtime_error("Field value is default");
+		}
+
+		if (m_field.getType() != INT)
+		{
+			throw std::runtime_error("Field type isn't integer");
+		}
+
+		return m_intValue;
+	}
+
+	double FieldValue::getDoubleValue() const
+	{
+		if (isNull())
+		{
+			throw std::runtime_error("Field value is null");
+		}
+
+		if (isDefault())
+		{
+			throw std::runtime_error("Field value is default");
+		}
+
+		if (m_field.getType() != DOUBLE)
+		{
+			throw std::runtime_error("Field type isn't double");
+		}
+
+		return m_doubleValue;
+	}
+
+	std::string FieldValue::getStringValue() const
+	{
+		if (isNull())
+		{
+			throw std::runtime_error("Field value is null");
+		}
+
+		if (isDefault())
+		{
+			throw std::runtime_error("Field value is default");
+		}
+
+		if (m_field.getType() != STRING)
+		{
+			throw std::runtime_error("Field type isn't string");
+		}
+
+		return m_stringValue;
+	}
+
+	std::chrono::system_clock::time_point FieldValue::getDateTimeValue() const
+	{
+		if (isDefault())
+		{
+			throw std::runtime_error("Field value is default");
+		}
+
+		if (m_field.getType() == DATETIME)
+		{
+			return m_dateTimeValue;
+		}
+
+		if (m_field.getType() != STRING)
+		{
+			throw std::runtime_error("Field type isn't datetime");
+		}
+
+		if (!isNull())
+		{
+			return stringISOToDateTime(m_stringValue);
+		}
+		
+		return std::chrono::system_clock::time_point {};
+	}
+
+	IBinaryValue& FieldValue::getBinaryValue() const
+	{
+		if (isNull())
+		{
+			throw std::runtime_error("Field value is null");
+		}
+
+		if (isDefault())
+		{
+			throw std::runtime_error("Field value is default");
+		}
+
+		if (m_field.getType() != BINARY)
+		{
+			throw std::runtime_error("Field type isn't binary");
+		}
+
+		return *m_binaryValue.get();
+	}
+
+	void FieldValue::setValue(const IFieldValue& srcFieldValue)
+	{
+		const FieldTypes srcFieldType = srcFieldValue.getField().getType();
+		if (srcFieldType != getField().getType())
+		{
+			throw std::runtime_error("Can't set the value of a field of another type");
+		}
+
+		if (srcFieldValue.isNull())
+		{
+			setNull();
+		}
+		else if (srcFieldValue.isDefault())
+		{
+			setDefault();
+		}
+		else
+		{
+			switch (srcFieldType)
+			{
+				case BOOLEAN:
+					setBooleanValue(srcFieldValue.getBooleanValue());
+					break;
+				case INT:
+					setIntValue(srcFieldValue.getIntValue());
+					break;
+				case DOUBLE:
+					setDoubleValue(srcFieldValue.getDoubleValue());
+					break;
+				case STRING:
+					setStringValue(srcFieldValue.getStringValue());
+					break;
+				case DATETIME:
+					setDateTimeValue(srcFieldValue.getDateTimeValue());
+					break;
+				case BINARY:
+					//setBinaryValue(srcFieldValue.getBinaryValue()); // Not implemented
+					break;
+			}
+		}
+	}
+
+	void FieldValue::setNull()
+	{
+		m_nullValue = true;
+		m_default = false;
+		m_boolValue = false;
+		m_intValue = 0;
+		m_doubleValue = 0.;
+		m_stringValue = "";
+		m_dateTimeValue = std::chrono::system_clock::time_point {};
+		m_binaryValue.reset();
+	}
+
+	void FieldValue::setDefault()
+	{
+		m_nullValue = false;
+		m_default = true;
+		m_boolValue = false;
+		m_intValue = 0;
+		m_doubleValue = 0.;
+		m_stringValue = "";
+		m_dateTimeValue = std::chrono::system_clock::time_point {};
+		m_binaryValue.reset();
+	}
+
+	void FieldValue::setBooleanValue(bool value)
+	{
+		if (m_field.getType() != BOOLEAN)
+		{
+			throw std::runtime_error("Field type isn't boolean");
+		}
+
+		m_boolValue = value;
+		m_nullValue = false;
+		m_default = false;
+	}
+
+	void FieldValue::setIntValue(int value)
+	{
+		if (m_field.getType() != INT)
+		{
+			throw std::runtime_error("Field type isn't integer");
+		}
+		
+		m_intValue = value;
+		m_nullValue = false;
+		m_default = false;
+	}
+
+	void FieldValue::setDoubleValue(double value)
+	{
+		if (m_field.getType() != DOUBLE)
+		{
+			throw std::runtime_error("Field type isn't double");
+		}
+
+		m_doubleValue = value;
+		m_nullValue = false;
+		m_default = false;
+	}
+
+	void FieldValue::setStringValue(const std::string& value)
+	{
+		if (m_field.getType() != STRING)
+		{
+			throw std::runtime_error("Field type isn't string");
+		}
+		
+		m_stringValue = value;
+		m_nullValue = false;
+		m_default = false;
+	}
+
+	void FieldValue::setDateTimeValue(const std::chrono::system_clock::time_point& value)
+	{
+		if (m_field.getType() != DATETIME)
+		{
+			throw std::runtime_error("Field type isn't datetime");
+		}
+		
+		m_dateTimeValue = value;
+		m_nullValue = isDateTimeNull(value);
+		m_default = false;
+	}
+
+	void FieldValue::setBinaryValue(std::unique_ptr<IBinaryValue> value)
+	{
+		if (m_field.getType() != BINARY)
+		{
+			throw std::runtime_error("Field type isn't binary");
+		}
+		
+		m_binaryValue = std::move(value);
+		m_nullValue = false;
+		m_default = false;
+	}
+
+	void FieldValue::useDefaultValue()
+	{
+		const FieldTypes fieldType = m_field.getType();
+		switch (fieldType)
+		{
+			case BOOLEAN:
+				setBooleanValue(m_field.getBooleanDefaultValue());
+				break;
+
+			case INT:
+				setIntValue(m_field.getIntDefaultValue());
+				break;
+
+			case DOUBLE:
+				setDoubleValue(m_field.getDoubleDefaultValue());
+				break;
+
+			case STRING:
+				setStringValue(m_field.getStringDefaultValue());
+				break;
+
+			case DATETIME:
+				setDateTimeValue(m_field.getDateTimeDefaultValue());
+				break;
+
+			case BINARY:
+				throw std::runtime_error("Binary field type not implemented.");
+
+			default:
+				throw std::runtime_error("Invalid field type.");
+				break;
+		}
+	}
+
+	std::unique_ptr<IFieldValue> FieldValue::clone() const
+	{
+		if (isNull())
+		{
+			return std::unique_ptr<IFieldValue>(new FieldValue(m_field));
+		}
+		
+		if (isDefault())
+		{
+			std::unique_ptr<IFieldValue> fieldValue(new FieldValue(m_field));
+			fieldValue->setDefault();
+			return fieldValue;
+		}
+
+		const FieldTypes fieldType = m_field.getType();
+		switch (fieldType)
+		{
+			case BOOLEAN:
+				return std::unique_ptr<IFieldValue>(new FieldValue(m_field, m_boolValue));
+
+			case INT:
+				return std::unique_ptr<IFieldValue>(new FieldValue(m_field, m_intValue));
+
+			case DOUBLE:
+				return std::unique_ptr<IFieldValue>(new FieldValue(m_field, m_doubleValue));
+
+			case STRING:
+				return std::unique_ptr<IFieldValue>(new FieldValue(m_field, m_stringValue));
+
+			case DATETIME:
+				return std::unique_ptr<IFieldValue>(new FieldValue(m_field, m_dateTimeValue));
+
+			case BINARY:
+				throw std::runtime_error("Binary field type not implemented.");
+
+			default:
+				throw std::runtime_error("Invalid field type.");
+				break;
+		}
+	}
+}
---
+++ src/DbPostgreSQLAdapter/FieldValue.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "DbAdapterInterface/IFieldValue.h"
+#include "DbAdapterInterface/IBinaryValue.h"
+
+namespace systelab::db {
+	class IField;
+}
+
+namespace systelab::db::postgresql {
+
+	class FieldValue : public IFieldValue
+	{
+	public:
+
+		FieldValue(const IField&);
+		FieldValue(const IField&, bool);
+		FieldValue(const IField&, int);
+		FieldValue(const IField&, double);
+		FieldValue(const IField&, const std::string&);
+		FieldValue(const IField&, const std::chrono::system_clock::time_point&);
+		FieldValue(const IField&, std::unique_ptr<IBinaryValue>);
+		~FieldValue(void) override;
+
+		const IField& getField() const override;
+
+		bool isNull() const override;
+		bool isDefault() const override;
+		bool getBooleanValue() const override;
+		int getIntValue() const override;
+		double getDoubleValue() const override;
+		std::string getStringValue() const override;
+		std::chrono::system_clock::time_point getDateTimeValue() const override;
+		IBinaryValue& getBinaryValue() const override;
+
+		void setValue(const IFieldValue&) override;
+		void setNull() override;
+		void setDefault();
+		void setBooleanValue(bool value) override;
+		void setIntValue(int value) override;
+		void setDoubleValue(double value) override;
+		void setStringValue(const std::string& value) override;
+		void setDateTimeValue(const std::chrono::system_clock::time_point& value) override;
+		void setBinaryValue(std::unique_ptr<IBinaryValue> value) override;
+
+		void useDefaultValue();
+
+		std::unique_ptr<IFieldValue> clone() const;
+
+	private:
+		const IField& m_field;
+
+		bool m_nullValue;
+		bool m_default;
+		bool m_boolValue;
+		int m_intValue;
+		double m_doubleValue;
+		std::string m_stringValue;
+		std::chrono::system_clock::time_point m_dateTimeValue;
+		std::unique_ptr<IBinaryValue> m_binaryValue;
+	};
+}
