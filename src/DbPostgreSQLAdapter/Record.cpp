217ece7050f0995befce88c8cffd75f06e16073d
jlpeiro <jpeiro@werfen.com>
2024-02-14 15:42:44 +0100

    Created implementation of Record interface

---
+++ src/DbPostgreSQLAdapter/Record.cpp
@@ -0,0 +1,114 @@
+#include "stdafx.h"
+#include "Record.h"
+
+#include "Field.h"
+#include "FieldValue.h"
+
+#include "DbAdapterInterface/IRecordSet.h"
+
+#include "libpq-fe.h"
+
+namespace {
+	std::chrono::system_clock::time_point stringISOToDateTime(const std::string& dateTime)
+	{
+		std::chrono::system_clock::time_point timePointDateTime;
+		std::istringstream{ dateTime } >> std::chrono::parse("%F %T%z", timePointDateTime);
+		return timePointDateTime;
+	}
+}
+
+namespace systelab::db::postgresql {
+
+	Record::Record(IRecordSet& recordSet, const PGresult* statementResult, const int rowIndex)
+	{
+		const unsigned int fieldsCount = recordSet.getFieldsCount();
+		for (unsigned int i = 0; i < fieldsCount; i++)
+		{
+			std::unique_ptr<IFieldValue> fieldValue;
+			const IField& field = recordSet.getField(i);
+			const unsigned int fieldIndex = field.getIndex();
+			if (PQgetisnull(statementResult, rowIndex, fieldIndex) == 1)
+			{
+				fieldValue.reset( new FieldValue(field));
+			}
+			else
+			{
+				FieldTypes fieldType = field.getType();
+				const std::string value = PQgetvalue(statementResult, rowIndex, fieldIndex);
+				switch(fieldType)
+				{
+					case BOOLEAN:
+						fieldValue = std::make_unique<FieldValue>(field, (value == "t"));
+						break;
+					case INT:
+						fieldValue = std::make_unique<FieldValue>(field, std::stoi(value));
+						break;
+					case DOUBLE:
+						fieldValue = std::make_unique<FieldValue>(field, std::stod(value));
+						break;
+					case STRING:
+						fieldValue = std::make_unique<FieldValue>(field, std::string(value));
+						break;
+
+					case DATETIME:
+						fieldValue = std::make_unique<FieldValue>(field, stringISOToDateTime(value));
+						break;
+					case BINARY:
+					default:
+						throw std::runtime_error( "Unknown field type." );
+				}
+			}
+
+			m_fieldValues.push_back(std::move(fieldValue));
+		}
+	}
+
+	Record::Record(std::vector<std::unique_ptr<IFieldValue>>& fieldValues)
+	{
+		const unsigned int nFieldValues = static_cast<unsigned int>(fieldValues.size());
+		for (unsigned int i = 0; i < nFieldValues; i++)
+		{
+			m_fieldValues.push_back(std::move(fieldValues.at(i)));
+		}
+	}
+
+	unsigned int Record::getFieldValuesCount() const
+	{
+		return static_cast<unsigned int>(m_fieldValues.size());
+	}
+
+	IFieldValue& Record::getFieldValue(unsigned int index) const
+	{
+		if (index >= m_fieldValues.size())
+		{
+			throw std::runtime_error("Invalid field value index");
+		}
+
+		return *(m_fieldValues.at(index).get());
+	}
+
+	IFieldValue& Record::getFieldValue(const std::string& fieldName) const
+	{
+		unsigned int nFields = (unsigned int) m_fieldValues.size();
+		for (unsigned int i = 0; i < nFields; i++)
+		{
+			if (m_fieldValues[i]->getField().getName() == fieldName)
+			{
+				return *(m_fieldValues[i].get());
+			}
+		}
+
+		throw std::runtime_error( "The requested field value doesn't exist" );
+	}
+
+	bool Record::hasFieldValue(const std::string& fieldName) const
+	{
+		const auto fieldValueIterator = std::find_if(m_fieldValues.cbegin(), m_fieldValues.cend(),
+			[&fieldName](const std::unique_ptr<IFieldValue>& fieldValue)
+			{
+				return fieldValue->getField().getName() == fieldName;
+			});
+
+		return fieldValueIterator != m_fieldValues.cend();
+	}
+}
---
+++ src/DbPostgreSQLAdapter/Record.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "DbAdapterInterface/IRecord.h"
+
+typedef struct pg_result PGresult;
+
+namespace systelab::db {
+	class IFieldValue;
+	class IRecordSet;
+}
+
+namespace systelab::db::postgresql {
+
+	class Record : public IRecord
+	{
+	public:
+		Record(IRecordSet& recordSet, const PGresult* statementResult, const int rowIndex);
+		Record(std::vector<std::unique_ptr<IFieldValue>>&);
+		~Record() override = default;
+
+		unsigned int getFieldValuesCount() const override;
+		IFieldValue& getFieldValue(unsigned int index) const override;
+		IFieldValue& getFieldValue(const std::string& fieldName) const override;
+
+		bool hasFieldValue(const std::string& fieldName) const override;
+
+	private:
+		std::vector<std::unique_ptr<IFieldValue>> m_fieldValues;
+	};
+}
