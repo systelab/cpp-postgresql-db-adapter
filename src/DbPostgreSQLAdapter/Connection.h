bf0fc391a05cee1dc429dee6d8fe945f85986d50
jlpeiro <jpeiro@werfen.com>
2024-02-19 14:58:54 +0100

    Simplified PostgreSQLException definition

--- src/DbPostgreSQLAdapter/Connection.h
+++ src/DbPostgreSQLAdapter/Connection.h
@@ -22,18 +22,8 @@ namespace systelab { namespace db { namespace postgresql {
 		struct PostgreSQLException : public Exception
 		{
 			PostgreSQLException(const std::string& message, const std::string& extendedMessage)
-				: Exception(message)
-				, m_extendedMessage(extendedMessage)
+				: Exception(message + ": " + extendedMessage)
 			{}
-
-			virtual const char* what() const noexcept override
-			{
-				std::ostringstream oss;
-				oss << std::runtime_error::what() << ": " << m_extendedMessage << std::endl;
-				return oss.str().c_str();
-			}
-
-			std::string m_extendedMessage;
 		};
 	};
 }}}
