c124be6b048242c0b74689a4ac13f26913354272
jlpeiro <jpeiro@werfen.com>
2024-02-21 12:52:33 +0100

    Fixed requirements version on conanfile

--- conanfile.py
+++ conanfile.py
@@ -19,10 +19,8 @@ class DbPostgreSQLAdapterConan(ConanFile):
         self.requires("DbAdapterInterface/2.0.0@systelab/stable")
         self.requires("zlib/1.2.13#13c96f538b52e1600c40b88994de240f", override=True)
         self.requires("openssl/3.0.13#05ab04ecefd8822c9b50c84febe403b3", override=True)
-        self.requires("libpq/15.4@")
-
-    def build_requirements(self):
-         self.build_requires("gtest/1.14.0@")
+        self.requires("libpq/15.4#cbae5e1ee85bd5e959e039e00307e8b1")
+        self.requires("gtest/1.14.0#4372c5aed2b4018ed9f9da3e218d18b3", private=True)         
 
     def build(self):
         cmake = CMake(self)
